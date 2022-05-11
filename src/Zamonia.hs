ca4247c4008f1ceb526a734cb0c16704cfe35ed1
Luc-Saccoccio <lucsaccoccio@disroot.org>
2022-05-11 21:03:33 +0200

    - Formatting ? - localLocation will now use XDG_DATA_HOME if possible - Added works Location - Added editor lookup, falls back to "vi". Won't be necessary in the   future ?

--- src/Zamonia.hs
+++ src/Zamonia.hs
@@ -1,75 +1,76 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Zamonia
-    ( FilmsCommand(..)
-    , SeriesCommand(..)
-    , BooksCommand(..)
-    , Film(..)
-    , Series(..)
-    , Book(..)
-    , Sort(..)
-    , (<~>)
-    , localLocation
-    , connection
-
-    , listFilms
-    , listSeries
-    , listBooks
-
-    , delFilm
-    , delSeries
-    , delBook
-
-    , printFilm
-    , printSeries
-    , printBook
-
-    , addWork
-    , modWork
-
-    , importSeriesCSV
-    , importFilmsCSV
-    , importBooksCSV
-
-    , importSeriesJSON
-    , importFilmsJSON
-    , importBooksJSON
-
-    , exportSeriesCSV
-    , exportFilmsCSV
-    , exportBooksCSV
-
-    , exportSeriesJSON
-    , exportFilmsJSON
-    , exportBooksJSON
-
-    , seriesToFullFormatted
-    , filmsToFullFormatted
-    , booksToFullFormatted
-
-    , purgeFilms
-    , purgeSeries
-    , purgeBooks)
-    where
+  ( FilmsCommand(..)
+  , SeriesCommand(..)
+  , BooksCommand(..)
+  , Film(..)
+  , Series(..)
+  , Book(..)
+  , Sort(..)
+  , (<~>)
+  , localLocation
+  , booksLocation
+  , seriesLocation
+  , filmsLocation
+  , connection
+  , listFilms
+  , listSeries
+  , listBooks
+  , delFilm
+  , delSeries
+  , delBook
+  , printFilm
+  , printSeries
+  , printBook
+  , addWork
+  , modWork
+  , importCSV
+  , importJSON
+  , exportCSV
+  , exportJSON
+  , allToFullFormatted
+  , purgeFilms
+  , purgeSeries
+  , purgeBooks
+  ) where
 
 import           Control.Exception      (bracket)
 import           Data.Functor           ((<&>))
+import           Data.Maybe             (fromMaybe)
 import           Database.SQLite.Simple
-import           System.Environment     (getEnv)
+import           System.Environment     (getEnv, lookupEnv)
+import           Zamonia.Book
 import           Zamonia.Film
 import           Zamonia.Series
-import           Zamonia.Book
 import           Zamonia.Work
 
 
 -- | Location of zamonia "home" for files
 localLocation :: IO FilePath
-localLocation = getEnv "HOME" <&> (++ "/.local/share/zamonia")
+localLocation = maybe (getEnv "HOME" <&> (++ "/.local/share/zamonia")) (return . (++ "/zamonia")) =<< lookupEnv "XDG_DATA_HOME"
+
+-- | Location of Books commentary
+booksLocation :: IO FilePath
+booksLocation = localLocation <&> (++ "/Books/")
+
+-- | Location of Series commentary
+seriesLocation :: IO FilePath
+seriesLocation = localLocation <&> (++ "/Series/")
+
+-- | Location of Films commentary
+filmsLocation :: IO FilePath
+filmsLocation = localLocation <&> (++ "/Films/")
 
 -- | Location of the database
 databaseLocation :: IO FilePath
 databaseLocation = localLocation <&> (++ "/zamonia.db")
 
+-- | The editor to use. Default to "vi"
+editor :: IO String
+editor = lookupEnv "EDITOR" <&> fromMaybe "vi"
+
 -- | Trying to open a database and returning the Connection.
 -- It takes a function. If the function fails, connection closes the connection to the database.
 connection :: (Connection -> IO c) -> IO c
-connection = bracket (open =<< databaseLocation) (\c -> execute_ c "PRAGMA optimize" >> close c)
+connection = bracket (open =<< databaseLocation)
+                     (\c -> execute_ c "PRAGMA optimize" >> close c)
